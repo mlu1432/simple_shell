9eb79cbec8f6128522519025b54f5e2a13e8ba62
oyeniran samuel sanmi <sossesam@gmail.com>
2023-07-26 08:42:52 +0100

    all

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<string.h>
+
+
+
+int _putchar(char c);
+int _strlen(char *string);
+void printline(char *string);
+int display_prompt(void);
+int file_exist(char *string);
+char *get_path(char *filename);
+void exec_cmd(char **filepath);
+char **con_args(char *string, char *delim);
+
+
+
+
+
+
+#endif
