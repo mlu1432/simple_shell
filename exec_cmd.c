decc5729471ba87cd38647264422fb11444a1e0f
oyeniran samuel sanmi <sossesam@gmail.com>
2023-07-26 15:59:27 +0100

    all

--- exec_cmd.c
+++ exec_cmd.c
@@ -3,10 +3,12 @@
 void exec_cmd(char **filepath){
        int id;
        int status;
+        status = 0;
        id = fork();
        if(id == 0){
         if(execve(filepath[0], filepath, NULL) == -1)
                      perror("ERROR");
+                     exit(1);
        }else{
               wait(&status);
        }
