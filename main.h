942cd02bad299afc7ada42fd9355d49267f7780a
oyeniran samuel sanmi <sossesam@gmail.com>
2023-07-28 01:45:08 +0100

    all

--- main.h
+++ main.h
@@ -1,25 +1,33 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <sys/types.h>
-#include <unistd.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include<string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <time.h>
 
 extern char **environ;
 
+int _strcmp(char *str1, char *str2);
+int verify_path(char **arguments);
+char *_strdup(char *str);
+char *append_command(char *dir_path, char *command);
+int exec(char **arguments);
+int fill_args(char *entry, char **arguments);
+int print_numbers(int n);
+int _strlen(const char *string);
+int print_not_found(char **arguments, int counter);
+char *_getenv(char *global_var);
+int _printp(const char *prompt, unsigned int size);
 int _putchar(char c);
-int _strlen(char *string);
-void printline(char *string);
-int display_prompt(void);
-int file_exist(char *string);
-char *get_path(char *filename);
-void exec_cmd(char **filepath, char **environ);
-char **con_args(char *string, char *delim);
-void free_pointer(char **arr);
-int launch(char *buffer);
+int exist(char *pathname);
+void free_grid(char **grid, int heigth);
+void last_free(char *entry);
+int verify_blt(char **arguments, int exit_stat);
 
 #endif
